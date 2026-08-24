a60d322711733be3c2926e6fa92952d895efdf80
VvSeanGTvV <64627966+VvSeanGTvV@users.noreply.github.com>
2026-08-24 11:34:48 -0700

    updated comment

--- CIS-001/TheMaze.py
+++ CIS-001/TheMaze.py
@@ -8,17 +8,16 @@ def turn_right():
 def main():
     # Problem 3 — Maze Solving
     # Works with Maze & Corridor Exercise
-    # Compacted clearing if statements.
-    while no_beepers_present(): # Looping unless standing on one
-        if front_is_clear():
+    # Compacted clearing if statements. Using elif (else if)
+    while no_beepers_present(): # Loop till stand on one
+        if front_is_clear(): # Move when clear
             move()
-        elif left_is_clear():
+        elif left_is_clear(): # Turn 90
             turn_left()
-        elif right_is_clear():
+        elif right_is_clear(): # Turn 270
             turn_right()
-        elif back_is_clear():
-            turn_right()
+        elif back_is_clear(): # Turn 180
+            turn_left()
+            turn_left()
     if beepers_present(): # Picks it up whenever it is standing on one
         pick_beeper()
-            
-        
